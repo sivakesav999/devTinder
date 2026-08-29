0801b5289ceea14bb2e039a6bde5a5c529a539af
sivakesav999 <sivakesav999@gmail.com>
2026-08-29 15:22:19 +0530

    handling multiple routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
-const express = require('express');
+const express = require("express");
+
 const app = express();
 
-app.use(("/test"), (req, res) => {
-    res.send("Hello World");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    next();
+    res.send("User route 1");
+    
+  },
+  (req, res, next) => {
+    next();
+  },
+  (req, res, next) => {
+    next();
+    res.send("User route 2");
+    
+  },
+  (req, res, next) => {
+    res.send("User route 3");
+    next();
+  },
+);
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+  console.log("Server is running on port 3000");
 });
