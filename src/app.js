54cdab19fea18643899b84f677f3e91377976b1a
sivakesav999 <sivakesav999@gmail.com>
2026-09-08 14:10:34 +0530

    updated package.json

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const dns = require("dns");
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
+// const dns = require("dns");
+// dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require("express");
 const app = express();
